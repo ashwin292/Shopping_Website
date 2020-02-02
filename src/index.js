f3a97c16b68a8080676bf047d5cb05ddfc4a3b93
ashjsr60@gmail.com <ashwin@coursera_something.com>
2020-02-03 00:34:44 +0530

    Added Payment Method

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { store, persistor } from './redux/store'
 
 import './index.css';
 import App from './App';
-
-import store from './redux/store'
 
 ReactDOM.render(
     <Provider store={store}>
     {/* <App /> is wrapped between <BrowserRouter> so that
     it gets all of the functionality of routing */}
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
 document.getElementById('root'));
